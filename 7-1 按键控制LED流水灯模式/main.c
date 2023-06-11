9563bebf4b67157c83180aeb98c55db2cde472b3
linzhenghao233 <1392872316@qq.com>
2023-06-11 23:09:15 +0800

    0611

--- 7-1 按键控制LED流水灯模式/main.c
+++ 7-1 按键控制LED流水灯模式/main.c
@@ -1,50 +1,53 @@
 #include <REGX52.h>
+#include "Timer0.h"
+#include "Key.h"
+#include <INTRINS.h>
 
-void Timer0_Init();
+//void Timer0_Init();
+unsigned char KeyNum, LEDMode = 0;
 
 int main(void){
-	Timer0_Init();
+	P2 = 0xFE;
+	Timer0Init();
 	while(1){
-		
+		KeyNum = Key();
+		if(KeyNum){
+			if(KeyNum == 1){
+				LEDMode++;
+				if(LEDMode >= 2)LEDMode = 0;
+			}
+		}
 	}
 }
 
-void Timer0Init(void)		//1毫秒@11.0592MHz
-{
-	AUXR |= 0x80;		//定时器时钟1T模式
-	TMOD &= 0xF0;		//设置定时器模式
-	TMOD |= 0x01;		//设置定时器模式
-	TL0 = 0xCD;		//设置定时初值
-	TH0 = 0xD4;		//设置定时初值
-	TF0 = 0;		//清除TF0标志
-	TR0 = 1;		//定时器0开始计时
-}
-
-void Timer0_Init(){
-	//TMOD = 0x01;	//0000 0001，十六位
-	TMOD &= 0xF0;	//把TMOD的低四位清零，高四位保持不变
-	TMOD |= 0x01;	//把TMOD的最低位置一，高四位保持不变
-	TF0 = 0;	//溢出中断标志
-	TR0 = 1;	//开始工作
-	/*一个寄存器存8位（2的8次方等于256），要存65535即需16位，此时就把
-	两个寄存器拼起来，从而达到存16位的目的。
-	TL0存256以下的数，TH0存以上的*/
-	TH0 = 64535 / 256;
-	TL0 = 64535 % 256;
-	
-	//中断器启动
-	ET0 = 1;
-	EA = 1;
-	PT0 = 0;
-}
-
-unsigned int T0Count = 0;
 void Timer0_Routine() interrupt 1{
-	TH0 = 64535 / 256;
-	TL0 = 64535 % 256;
+	static unsigned int T0Count = 0;
+	TH0 = 0xFC;
+	TL0 = 0x66;
 	T0Count++;
-	if(T0Count >= 1000){
+	if(T0Count >= 500){
 		T0Count = 0;
-		P2_0 = ~P2_0;
+		if(LEDMode == 0)
+			P2 = _crol_(P2,1);
+		if(LEDMode == 1)
+			P2 = _cror_(P2,1);
 	}
 }
+
+//void Timer0_Init(){
+//	//TMOD = 0x01;	//0000 0001，十六位
+//	TMOD &= 0xF0;	//把TMOD的低四位清零，高四位保持不变
+//	TMOD |= 0x01;	//把TMOD的最低位置一，高四位保持不变
+//	TF0 = 0;	//溢出中断标志
+//	TR0 = 1;	//开始工作
+//	/*一个寄存器存8位（2的8次方等于256），要存65535即需16位，此时就把
+//	两个寄存器拼起来，从而达到存16位的目的。
+//	TL0存256以下的数，TH0存以上的*/
+//	TH0 = 64535 / 256;
+//	TL0 = 64535 % 256;
+//	
+//	//中断器启动
+//	ET0 = 1;
+//	EA = 1;
+//	PT0 = 0;
+//}
---
+++ 7-2 定时器时钟/main.c
@@ -0,0 +1,40 @@
+#include <REGX52.h>
+#include "Delay.h"
+#include "LCD1602.h"
+#include "Timer0.h"
+
+unsigned char second = 0, min = 0, hour = 0;
+
+int main(void){
+	LCD_Init();
+	Timer0Init();
+	
+	LCD_ShowString(1, 1, "Clock:");
+	LCD_ShowString(2, 1, "  :  :");
+	while(1){
+		LCD_ShowNum(2, 1, hour, 2);
+		LCD_ShowNum(2, 4, min, 2);
+		LCD_ShowNum(2, 7, second, 2);
+	}
+}
+
+void Timer0_Routine() interrupt 1{
+	static unsigned int T0Count = 0;
+	TH0 = 0xFC;
+	TL0 = 0x66;
+	T0Count++;
+	if(T0Count >= 1000){
+		T0Count = 0;
+		second++;
+		if(second == 60){
+			second = 0;
+			min++;
+		}
+		if(min == 60){
+			min = 0;
+			hour++;
+		}
+		if(hour = 24)
+			hour = 0;
+	}
+}
