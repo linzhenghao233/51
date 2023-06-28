0454408b6e428d622bd4654f65c7f4a338fc3d5c
linzhenghao233 <1392872316@qq.com>
2023-06-28 23:38:27 +0800

    0628

---
+++ 9-1 LED点阵屏显示图形/main.c
@@ -0,0 +1,17 @@
+#include <REGX52.h>
+
+sbit RCK = P3^5;	//RCLK
+sbit SCK = P3^6;	//SRCLK
+sbit SER = P3^4;	//SER
+
+void _74HC595_WriteByte(unsigned char Byte);
+
+int main(void){
+	while(1){
+		
+	}
+}
+
+void _74HC595_WriteByte(unsigned char Byte){
+	
+}
