9e6d9b86d09703595c9d69137989af1e83bbe41c
linzhenghao233 <1392872316@qq.com>
2023-07-12 21:55:22 +0800

    0712

--- 10-1 DS1302时钟/DS1302.c
+++ 10-1 DS1302时钟/DS1302.c
@@ -4,6 +4,17 @@ sbit DS1302_SCLK = P3^6;
 sbit DS1302_IO = P3^4;
 sbit DS1302_CE = P3^5;
 
+#define DS1302_SECOND		0x80
+#define DS1302_MINUTE		0x82
+#define DS1302_HOUR			0x84
+#define DS1302_DATE			0x86
+#define DS1302_MONTH		0x88
+#define DS1302_DAY			0x8A
+#define DS1302_YEAR			0x8C
+#define DS1302_WP				0x8E
+
+unsigned char DS1302_Time[] = {23, 7, 12, 17, 59, 55, 3};
+
 void DS1302_Init(void){
 	DS1302_SCLK = 0;
 	DS1302_CE = 0;
@@ -30,6 +41,7 @@ void DS1302_WriteByte(unsigned char Command, Data){
 
 unsigned char DS1302_ReadByte(unsigned char Command){
 	unsigned char i, Data = 0x00;
+	Command |= 0x01;
 	DS1302_CE = 1;
 	
 	for(i = 0; i < 8; i++){
@@ -45,4 +57,34 @@ unsigned char DS1302_ReadByte(unsigned char Command){
 	DS1302_CE = 0;
 	DS1302_IO = 0;
 	return Data;
+}
+
+void DS1302_SetTime(void){
+	DS1302_WriteByte(DS1302_WP, 0x00);
+	DS1302_WriteByte(DS1302_YEAR, DS1302_Time[0] / 10 * 16 + DS1302_Time[0] % 10);
+	DS1302_WriteByte(DS1302_MONTH, DS1302_Time[1] / 10 * 16 + DS1302_Time[1] % 10);
+	DS1302_WriteByte(DS1302_DATE, DS1302_Time[2] / 10 * 16 + DS1302_Time[2] % 10);
+	DS1302_WriteByte(DS1302_HOUR, DS1302_Time[3] / 10 * 16 + DS1302_Time[3] % 10);
+	DS1302_WriteByte(DS1302_MINUTE, DS1302_Time[4] / 10 * 16 + DS1302_Time[4] % 10);
+	DS1302_WriteByte(DS1302_SECOND, DS1302_Time[5] / 10 * 16 + DS1302_Time[5] % 10);
+	DS1302_WriteByte(DS1302_DAY, DS1302_Time[6] / 10 * 16 + DS1302_Time[6] % 10);
+	DS1302_WriteByte(DS1302_WP, 0x80);
+}
+
+void DS1302_ReadTime(void){
+	unsigned char Temp;
+	Temp = DS1302_ReadByte(DS1302_YEAR);
+	DS1302_Time[0] = Temp / 16 * 10 + Temp % 16;
+	Temp = DS1302_ReadByte(DS1302_MONTH);
+	DS1302_Time[1] = Temp / 16 * 10 + Temp % 16;
+	Temp = DS1302_ReadByte(DS1302_DATE);
+	DS1302_Time[2] = Temp / 16 * 10 + Temp % 16;
+	Temp = DS1302_ReadByte(DS1302_HOUR);
+	DS1302_Time[3] = Temp / 16 * 10 + Temp % 16;
+	Temp = DS1302_ReadByte(DS1302_MINUTE);
+	DS1302_Time[4] = Temp / 16 * 10 + Temp % 16;
+	Temp = DS1302_ReadByte(DS1302_SECOND);
+	DS1302_Time[5] = Temp / 16 * 10 + Temp % 16;
+	Temp = DS1302_ReadByte(DS1302_DAY);
+	DS1302_Time[6] = Temp / 16 * 10 + Temp % 16;
 }
--- 10-1 DS1302时钟/main.c
+++ 10-1 DS1302时钟/main.c
@@ -1,19 +1,22 @@
 #include <REGX51.h>
 #include "LCD1602.h"
 #include "DS1302.h"
-#include "Delay.h"
-
-unsigned char Second;
 
 int main(void){
 	LCD_Init();
 	DS1302_Init();
-	LCD_ShowString(1, 1, "hello");
-	DS1302_WriteByte(0x8E, 0x00);//解除写入保护
-	DS1302_WriteByte(0x80, 0x03);
+	LCD_ShowString(1, 1, "  -  -  ");
+	LCD_ShowString(2, 1, "  :  :  ");
+	
+	DS1302_SetTime();
 
 	while(1){
-		Second = DS1302_ReadByte(0x81);
-		LCD_ShowHexNum(2, 1, Second, 3);
+		DS1302_ReadTime();
+		LCD_ShowNum(1, 1, DS1302_Time[0], 2);
+		LCD_ShowNum(1, 4, DS1302_Time[1], 2);
+		LCD_ShowNum(1, 7, DS1302_Time[2], 2);
+		LCD_ShowNum(2, 1, DS1302_Time[3], 2);
+		LCD_ShowNum(2, 4, DS1302_Time[4], 2);
+		LCD_ShowNum(2, 7, DS1302_Time[5], 2);
 	}
 }
