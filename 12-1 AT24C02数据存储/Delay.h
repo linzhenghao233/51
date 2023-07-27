c167ecf7556c5bab0db63a61ac7c87ce8f6e64bc
linzhenghao233 <1392872316@qq.com>
2023-07-27 22:01:43 +0800

    0727

---
+++ 12-1 AT24C02数据存储/Delay.h
@@ -0,0 +1,6 @@
+#ifndef __DELAY_H__
+#define __DELAY_H__
+
+void Delay(unsigned int time_ms);
+
+#endif
---
+++ 12-1 AT24C02数据存储/main.c
@@ -0,0 +1,12 @@
+#include <REGX52.h>
+#include "Delay.h"
+#include "Key.h"
+#include "LCD1602.h"
+
+int main(void){
+	LCD_Init();
+	LCD_ShowString(1, 1, "Hello");
+	while(1){
+		
+	}
+}
